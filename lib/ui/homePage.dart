c1c2dcb91e2e83a9a0fdd87b33d910212a1bfc07
Yara Horany <yara.horany1@gmail.com>
2023-04-30 11:02:40 +0300

    Adding texts

--- lib/ui/homePage.dart
+++ lib/ui/homePage.dart
@@ -10,6 +10,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            flex: MediaQuery.of(context).size.height.toInt() ~/ 136.685,
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.green,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text(
+                    'Score',
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      _play();
+                    },
+                    child: const Text(
+                      'play',
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+
+  _play() {}
 }
